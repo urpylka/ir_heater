bf3b2dd059481c8ca2fd63b2bd5475a6585ed328
Artem Smirnov <urpylka@gmail.com>
2023-05-06 23:08:51 +0300

    first commit

---
+++ src/config.h
@@ -0,0 +1,7 @@
+#ifndef config_H_
+#define config_H_
+
+#define var1 "value1"
+#define var2 "value2"
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+void setup()
+{
+    // pass
+}
+void loop()
+{
+    // pass
+}
